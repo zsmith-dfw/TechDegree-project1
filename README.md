37a884e1f9fba2aed6ba40131fce377a7bc6bb3e
Zach Smith <rysskismith@gmail.com>
2020-05-20 16:44:08 -0500

    final copy ready for submisison

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TechDegree project1
  Random Quote Generator 
+
+This is my first ever project and something I hope to look back on in the future as a benchmark of where I started. I am going for "meets expectations", but plan on using this project later to build upon and experiment and possibly add new features. 
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
- 
- //quotes array 
+// This is a simple array of quote objects.  The ones said by real people only have the quote itself as well as who said it. The ones from film have the name of the film (citation) as well as the year it was released. For fictional characters, the actor/actress saying the quote in parentheses.
 
 let quotes = [
   {quote: 'My grandfather once told me that there were two kinds of people: those who do the work and those who take the credit. He told me to try to be in the first group; there was much less competition.', author: "Indira Gandhi"},
@@ -19,58 +18,45 @@ let quotes = [
   {quote: 'Laugh a lot. It burns a lot of calories.', author: "Jessica Simpson"},
   {quote: 'A martini. Shaken, not stirred.', author: "James Bond (Sean Connery)", citation: "Goldfinger", year: "1964"},
   {quote: 'Carpe diem. Seize the day, boys. Make your lives extraordinary', author: "John Keating (Robin Williams)", citation: "Dead Poets Society", year: "1989"},
-  {quote: 'Nobody puts Baby in a corner.', author: "Johnny Castle (Patrick Swayze)", citation: "Dirty Dancing", year: "1987"}
+  {quote: 'Nobody puts Baby in a corner.', author: "Johnny Castle (Patrick Swayze)", citation: "Dirty Dancing", year: "1987"},
+  {quote: 'You can\'t handle the truth!', author: "Col. Nathan R. Jessup (	Jack Nicholson)", citation: "Goldfinger", year: "1964"},
+  {quote: 'Show me the money!', author: "Rod Tidwell (Cuba Gooding Jr.)", citation: "Jerry McGuire", year: "1996"},
+  {quote: 'Mama always said life was like a box of chocolates. You never know what you\'re gonna get.', author: "Forrest Gump(Tom Hanks)", citation: "Forrest Gump", year: "1994"},
+  {quote: 'Gentlement, you can\'t fight in here! This is the War Room!', author: "President Merkin Muffley (Peter Sellers)", citation: "Dr. Strangelove", year: "1964"},
+  {quote: 'Greed, for lack of a better word, is good.', author: "Gordon Gekko (Michael Douglas)", citation: "Wall Street", year: "1987"},
+  {quote: "Surely you can\'t be serious... I am serious, and don\'t call me Shirley", author: "Ted Striker and Dr. Rumack", citation: "Airplane", year: "1980"}
+];
 
-]
-
-
-
- //getRandomQuote` function
+ // This funciton takes the length of the quote array (so that we don't have to count every single quote...) and multiplies it by a random number between 0 and up to but not including 1. Whatever quote has an index value that matches our returned value becomes our random quote
 
 function getRandomQuote(quoteList) {
-  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]; // this code snippet was provided by Jacob Relkin on stackoverflow.com to answer the question "How can I select a random value from this array using JavaScript?"
-  console.log(randomQuote);
+  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]; // this code snippet was provided by Jacob Relkin on stackoverflow.com to answer the question "How can I select a random value from this array using JavaScript?" asked by another user several years ago. I modified it to fit the needs of this project.
   return randomQuote; 
 };
 
-getRandomQuote();
+getRandomQuote(); // This is really only for testing purposes - it does not affect the program in any way. This was also a spot where I had console.log but it is now moved to the end.
 
-
-//printQuote function
-
+// The printQuote function starts off with no parameters because they are not necessary for this program. We define our two variables. QuoteCall stores our random quote from earlier and logs it for future use. The quoteString variable exists because we need it to hold our HTML string. If we used "quoteCall" for that, it would override our previous assignment and we don't want that.
+// We finish off the function with a conditional statement to see if our randomly selected code is a quotation that has a citation and year. If it does, then we print that out as well.
 
 function printQuote() {
- 
-let quoteCall = getRandomQuote();
+  let quoteCall = getRandomQuote();
+  let quoteString = `<p class = "quote">${quoteCall.quote}</p><p class ="source">${quoteCall.author}`; // Here we are checking our variable "quoteCall" and printing the "quote" and "author" properties associated with our random quote.
+   
+  // Since we are going to use template literals to join the conditional statement with our string, we can leave the second <p> tag open for now. This if statement checks to see if the properties "citation" and "year" exist and use template literals to join the strings.
   
+  if (quoteCall.hasOwnProperty('citation')) {
+      quoteString += `<span class = "citation">${quoteCall.citation}</span>`
+  };
 
- 
-let quoteString = `<p class = "quote">${quoteCall.quote}</p>  
-<p class ="source">${quoteCall.author}`;
-
-if (quotes.hasOwnProperty('citation')) {
-    quoteString += `<span class = "citation">${quoteCall.citation}</span>`
-  } else {
-    quoteString += `</p>`
-};
-
-if (quotes.hasOwnProperty('year')) {
-    quoteString += `<span class = "year">${quoteCall.year}</span></p>`
-  } else {
-    quoteString += `</p>`
-    
+  if (quoteCall.hasOwnProperty('year')) {
+      quoteString += `<span class = "year">${quoteCall.year}</span></p>`
+      console.log(quoteString); // This is really just a convenience for a developer to ensure what the user sees matches the console and to troubleshoot issues.
   }; 
 
+document.getElementById('quote-box').innerHTML = quoteString; // This finally prints our quote in accordance with the HTML element tags defined in the index.HTML document. 
 
+};
 
+document.getElementById('load-quote').addEventListener("click", printQuote, true); // This results in a new quote every time the user clicks the button
 
-
-
- 
-document.getElementById('quote-box').innerHTML = quoteString;
-
-}
-
-
-
-document.getElementById('load-quote').addEventListener("click", printQuote, true);
